5c03c6b0839c180f0828f4355b61e74823d57dbf
tuuyen09 <tu190901@gmail.com>
2023-03-29 14:33:12 +0700

    MoveablePoint

--- src/MoveablePoint/MoveablePoint.java
+++ src/MoveablePoint/MoveablePoint.java
@@ -1,2 +1,65 @@
-package MoveablePoint;public class MoveablePoint {
+package MoveablePoint;
+
+public class MoveablePoint extends Point {
+    private float xSpeed;
+    private float ySpeed;
+
+    public MoveablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint() {
+
+    }
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed() {
+        float[] arr = {xSpeed, ySpeed};
+        return arr;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " xSpeed = " + getXSpeed() + " ySpeed = " + getYSpeed();
+    }
+
+    public MoveablePoint move() {
+        super.setX(super.getX() + xSpeed);
+        super.setY(super.getY() + ySpeed);
+        return this;
+    }
+}
+class Test {
+    public static void main(String[] args) {
+        MoveablePoint obj = new MoveablePoint(3,4,5,5);
+        obj.move();
+        System.out.println(obj);
+    }
 }
--- src/MoveablePoint/Point.java
+++ src/MoveablePoint/Point.java
@@ -1,2 +1,49 @@
-package MoveablePoint;public class Point {
+package MoveablePoint;
+
+public class Point {
+    private float x;
+    private float y;
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Point(){
+
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    public float[] getXY (){
+        float[] arr = {x,y};
+        return arr;
+    }
+    public void setXY(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "x = " + x + " y = " + y;
+    }
+}
+class TestPoint{
+    public static void main(String[] args) {
+        Point obj = new Point();
+        System.out.println(obj);
+    }
 }

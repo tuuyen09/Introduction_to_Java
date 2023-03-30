38cd587d55f25031690fc0acca41209f80d1fd3f
tuuyen09 <tu190901@gmail.com>
2023-03-30 13:46:50 +0700

    Comparator

---
+++ src/Comparator/Circle.java
@@ -0,0 +1,41 @@
+package Comparator;
+
+public class Circle {
+    protected double radius;
+    protected String color;
+    protected boolean filled;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getArea (){
+        return radius * radius * Math.PI;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
---
+++ src/Comparator/ComparatorCircle.java
@@ -0,0 +1,12 @@
+package Comparator;
+
+import java.util.Comparator;
+
+public class ComparatorCircle implements Comparator<Circle> {
+    @Override
+    public int compare(Circle o1, Circle o2) {
+        if (o1.getRadius() > o2.getRadius()) return 1;
+        else if (o1.getRadius() < o2.getRadius()) return -1;
+        else return 0;
+    }
+}
